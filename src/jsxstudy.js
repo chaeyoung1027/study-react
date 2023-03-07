0558a2d3450760154438eb2bab10a9016ca1e4b4
chaeyoung1027 <rnldyal1027@naver.com>
2023-03-08 08:59:31 +0900

    삭제& 추가 commit

---
+++ src/jsxstudy.js
@@ -0,0 +1,4 @@
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));  //index.html에 있는 root를 빌려옴!
+root.render(<div>Hello</div>);  //Hello를 그려줌!

4950dc52861bf2ca941557a0d413bd5f140e19a8
chaeyoung1027 <rnldyal1027@naver.com>
2023-03-10 08:52:33 +0900

    const를 사용하기

--- src/sol_greeting.js
+++ src/sol_greeting.js
@@ -2,7 +2,8 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 function Greeting(props){
-    return <h1>Hello, {props.name}</h1>
+    const name = props.name
+    return <h1>Hello, {name}</h1>
 }
 
 ReactDOM.render(<Greeting name = "채영"/>, document.getElementById("root"))

a4de4270bd44c615226c4e29261041a117432dd8
chaeyoung1027 <rnldyal1027@naver.com>
2023-03-10 09:41:02 +0900

    highlight를 다른쪽으로 빼놓았다

--- src/person-profile.js
+++ src/person-profile.js
@@ -3,7 +3,8 @@ import React from 'react'
 
 // 함수 인자값을 전달받으면서 비구조화 할당 진행
 const PersonProfile = function(props) { //구조분해 할당을 나누지 않고 parameter로 지정
-    const {name, age, gender, profile, highlight} = props.person;
+    const {name, age, gender, profile} = props.person;
+    const highlight = props.highlight;
     return (
         <div className='person' style={highlight ? {color: 'red'} : null}>
             <h1>Profile</h1>
@@ -23,4 +24,4 @@ const myperson = {
 }
 //name = "채영" age = "19" gender = "female" profile="https://image.idus.com/image/files/21a8e18243894251b847211ec7bf1071_512.jpg" highlight
 
-ReactDOM.render(<PersonProfile person = {myperson} />, document.getElementById("root"))
+ReactDOM.render(<PersonProfile person = {myperson} highlight/>, document.getElementById("root"))

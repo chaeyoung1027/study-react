e1b9bd02869a45b819fcd5df4112cc0a00aae51b
chaeyoung1027 <rnldyal1027@naver.com>
2023-03-15 09:33:12 +0900

    줄바꿈 수정

--- src/state-study-1.js
+++ src/state-study-1.js
@@ -2,15 +2,15 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 const Counter = function(props) {
-// useState 함수(훅)을 사용하여 상태 및 상태 변경 함수 불러오기
-const [count, setCount] = useState(0)
-return (
-<div>
-<h1>{count}</h1>
-<button onClick={() => setCount(count + 1)}>증가</button>
-{/* Q) 감소 버튼 만들어보기 */}
-<button onClick={() => setCount(count -1)}>감소</button>
-</div>
-)
+    // useState 함수(훅)을 사용하여 상태 및 상태 변경 함수 불러오기
+    const [count, setCount] = useState(0)
+    return (
+        <div>
+        <h1>{count}</h1>
+        <button onClick={() => setCount(count + 1)}>증가</button>
+        {/* Q) 감소 버튼 만들어보기 */}
+        <button onClick={() => setCount(count -1)}>감소</button>
+        </div>
+    )
 }
 ReactDOM.render(<Counter />, document.getElementById("root"))

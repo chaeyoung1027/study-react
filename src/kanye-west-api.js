882d3812a8617386f1b7f4808596c6af2784549b
chaeyoung1027 <rnldyal1027@naver.com>
2023-04-21 09:53:06 +0900

    add arrow, number to kanty quote

--- src/kanye-west-api.js
+++ src/kanye-west-api.js
@@ -1,34 +1,56 @@
-import React, { useState, useEffect } from "react"
-import ReactDOM from "react-dom"
-
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 function KanyeItem(props) {
-    return(
-        <div>
-            quote: {props.quote}
-        </div>
-    )
+  return (
+    <div>
+      quote: {props.quote}
+    </div>
+  );
 }
 
-function KanyeApp(){
-    const [quote, setQuote] = useState(null);
-    const [toggle, setToggle] = useState(false);
+function KanyeApp() {
+  const [quotes, setQuotes] = useState([]); //quote를 담은 배열
+  const [toggle, setToggle] = useState(false);
+  const [index, setIndex] = useState(0);    //위치값
+
+  useEffect(() => {
+    fetch('https://api.kanye.rest')
+    .then(res => res.json())
+    .then(json => setQuotes(quotes =>{
+        let newQuotes = quotes.concat(json.quote)
+        setIndex(newQuotes.length-1)
+        return newQuotes
+    }))
+  }, [toggle])
+
+  let controller
+  if(index === 0){
+    controller = <div><button onClick={() => setIndex(i => i+1)}>▶</button>
+    </div>
+  }else if(index +1 === quotes.length){
+    controller = <div><button onClick={() => setIndex(i => i-1)}>◀</button></div>  
+  }else{
+    controller = <div>
+    <button onClick={() => setIndex(i => i-1)}>◀</button>
+        <button onClick={() => setIndex(i => i+1)}>▶</button>
+    </div>
+  }
+  if(quotes.length === 1) controller = null
+
+  return <div>
+    {
+        quotes.length >0 &&<div> 
+            <p>{index+1} / {quotes.length}</p>
+            <p>quote : {quotes[quotes.length-1]}</p>
+            {controller}
+            </div>
+    }
+    <button onClick = {() =>{
+        setToggle(t => !t)
+    }}>명언 불러오기</button>
     
-    useEffect(()=>{
-        fetch(`https://api.kanye.rest/`)
-        .then(res => res.json())
-        .then(json =>{
-            setQuote(json.quote)
-        })
-        }, [toggle])
 
-    return(
-        <div>
-            {quote===null ? null: <KanyeItem quote = {quote}/>}
-            <br></br>
-            <button onClick={() =>{setToggle(toggle => !toggle)}}>
-                칸예 명언 불러오기</button>
-            
-        </div>
-    )
+  </div>
 }
-ReactDOM.render(<KanyeApp />, document.getElementById("root"))
+
+ReactDOM.render(<KanyeApp />, document.getElementById("root"));
